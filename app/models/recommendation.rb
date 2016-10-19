b11320a50a7f2247bce89047dbf923f59481debb
Dinesh Matta <dinesh.matta@synechron.com>
2016-10-20 01:16:16 +0530

    Fixing crash issue forsome webpages, showing most relevance result

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -12,11 +12,14 @@ class Recommendation
 		## Get the topics from Alchemy API
 		results = AlchemyAPI::KeywordExtraction.new.search(url: url)
 
+		## Select the most relevant keyword with relevancy index >= 0.6
+		results.select! {|item| item["relevance"] >= "0.6"}
+
 		## parse the response and extract the keywords
 		keywords = results.collect {|item| item["text"]}
 
 		if(keywords.length > 0)
-			keywords = keywords.join('|')
+			keywords = keywords.join('|').tr(" ", "+")
 		end
 
 		return keywords
@@ -24,11 +27,14 @@ class Recommendation
 
 	def self.get_recommendations(keywords)
 		videos = Yt::Collections::Videos.new
-		videos_collection = videos.where(q: keywords, safe_search: 'none', order: 'viewCount')
+		videos_collection = videos.where(part: 'snippet', q: keywords, type: 'video', safe_search: 'none', order: 'viewCount')
 		p videos_collection.size
-		videos_collection.map {|video| { id: video.id, title: video.title, desription: video.description, published_at: video.published_at, thumbnail_url: video.thumbnail_url}}
-		videos = videos_collection.map {|video| { id: video.id, title: video.title, desription: video.description, published_at: video.published_at, thumbnail_url: video.thumbnail_url, view_count: video.view_count}}
-		return videos
+
+		videos_collection.map(&:id)
+		ids = videos_collection.map(&:id).take(10)
+		snippets = videos_collection.map(&:snippet).take(10)
+		collection = Hash[ids.zip(snippets.map {|i| i})]
+		return collection.collect {|key, video| {id: key, title: video.title, desription: video.description, published_at: video.published_at, thumbnail_url: video.thumbnail_url}}
 	end
 
 end
